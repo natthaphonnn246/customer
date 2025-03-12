7bf83678c630f702d982d0f326af0bcfd2463da9
natthaphonnn246 <natthaphonnn246@gmail.com>
2025-03-12 11:49:12 +0700

    ปรับหน้าlogin

--- resources/views/auth/login-tailwind.blade.php
+++ resources/views/auth/login-tailwind.blade.php
@@ -9,7 +9,7 @@
     <script src="https://code.jquery.com/jquery-3.7.1.js"></script> --}}
 
     @vite('resources/css/app.css')
-    <title>nntpn.com</title>
+    <title>cms.vmdrug</title>
 </head>
 
 <style>
@@ -24,7 +24,7 @@
 
 
     <section class="bg-gray-50" style="background-color:rgb(223, 223, 223);">
-        <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0" text>
             {{-- <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
                 <img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo">
                 nntpn.com   
@@ -32,9 +32,10 @@
             <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:border-gray-700" style="background-color:rgb(255, 255, 255);">
                 <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
                     <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl text-center dark:text-white" style="font-weight:500;">
-                        {{-- <img src="/profile/logo.svg"> --}}
+                        <img class="ms-14" style="width:70%;" src="/profile/cmsvmdrugpng copy.png">
+                        
                         {{-- <img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo"> --}}
-                        <span style="color:black;">nntpn.com </span> 
+                        {{-- <span style="color:black;">CMS</span>  --}}
                     </h1>
                     <form class="space-y-4 md:space-y-6" method="POST" action="{{ route('login') }}">
                         @csrf
@@ -61,7 +62,7 @@
                             <button type="submit" class="w-full text-white bg-primary-400 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-500 dark:hover:bg-blue-700 dark:focus:ring-primary-800" style="width:100px;">Login</button>
                         </ul>
                         <p class="text-sm font-light text-gray-500 dark:text-gray-400 text-center">
-                            &copy; 2025 nntpn.com All rights reserved
+                            &copy; 2025 cms.vmdrug.co.th All rights reserved
                         </p>
                     </form>
                 </div>
