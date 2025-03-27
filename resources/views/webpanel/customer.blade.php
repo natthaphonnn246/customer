40b139416b379df1f61c5f6bdda21bef1eb6be46
natthaphonnn246 <natthaphonnn246@gmail.com>
2025-03-27 20:39:04 +0700

    customer : page size

--- resources/views/webpanel/customer.blade.php
+++ resources/views/webpanel/customer.blade.php
@@ -276,7 +276,16 @@
         <h6 class="justifiy-content:center; mt-2 ms-4 mr-6" style="background-color:#cb4d4d; border-radius:20px; padding: 5px; color:#ffffff; font-weight:500;">{{$status_updated}}</h6>
         @endsection
         {{-- <img src="{{ url('/') }}/storage/certificates/img_certstore/1dcV3LQvU5DbAW2hVAMAwHyYLLng85K9aGq4TX47.jpg"> --}}
+
+        {{-- {{$_SERVER['REMOTE_ADDR'];}} --}}
+
+    
+
+
     <div class="contentArea">
+
+
+
         <div class="py-2">
             {{-- <span style="color: #8E8E8E;"><a href="/webpanel/admin" id="backLink">ข้อมูลแอดมิน (Admin)</a> / แบบฟอร์ม</span> --}}
         </div>
