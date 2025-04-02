c58a26e814ddfaa9f55816d5b461f3d12e53ce12
natthaphonnn246 <natthaphonnn246@gmail.com>
2025-04-02 19:10:16 +0700

    test : migration

--- database/migrations/2025_03_22_160946_add_check_login_to_users_table.php
+++ database/migrations/2025_03_22_160946_add_check_login_to_users_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //add;
+        //add test;
         Schema::table('users', function (Blueprint $table) {
             $table->dropColumn(['check_login', 'date_login']);
         });
